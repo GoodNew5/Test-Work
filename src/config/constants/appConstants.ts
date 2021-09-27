d72dba1b8910c9c721d4cc20648bc22a795a32d5
Александр Веселов <csome264@gmail.com>
2021-09-27 15:12:35 +0300

    fix: production build

--- src/config/constants/appConstants.ts
+++ src/config/constants/appConstants.ts
@@ -1,7 +1,7 @@
 import appBreakpoints from 'styles/breakpoints.module.scss'
 
 const appConstants = {
-  ROUTES: { HOME_PATH: '/' },
+  ROUTES: { HOME_PATH: `${process.env.PUBLIC_URL}/` },
   APP_BASE_THEME: 'APP_BASE_THEME',
   API_BASE_URL: process.env.REACT_APP_API_BASE_URL,
   TOKEN_STORAGE_KEY: 'access_token',
